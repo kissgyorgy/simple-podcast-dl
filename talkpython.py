166fa927efac2e21b43828e654d99f44a31d5463
Kiss György <kissgyorgy@me.com>
2018-03-15 03:11:51 +0100

    First version: one-thread download all episodes
    to a given directory. The directory can come from the --download-dir option, or
    from the DOWNLOAD_DIR environment variable. All episodes will be downloaded.
    Cannot resume download.
    ## To make a commit, type your commit message and press SUPER-ENTER. To cancel
    ## the commit, close the window. To sign off on the commit, press SUPER-S.
    ##
    ## You may also reference or close a GitHub issue with this commit.  To do so,
    ## type `#` followed by the `tab` key.  You will be shown a list of issues
    ## related to the current repo.  You may also type `owner/repo#` plus the `tab`
    ## key to reference an issue in a different GitHub repo.
    
    diff --git a/talkpython.py b/talkpython.py
    new file mode 100755
    index 0000000..dec3fc0
    --- /dev/null
    +++ b/talkpython.py
    @@ -0,0 +1,82 @@
    +#!/usr/bin/env python3.6
    +import os
    +import sys
    +import argparse
    +from pathlib import Path
    +import requests
    +from lxml import etree
    +
    +
    +RSS_URL = 'https://talkpython.fm/episodes/rss'
    +
    +
    +class Episode:
    +    def __init__(self, enclosure):
    +        self.url = enclosure.get('url')
    +        self.length = enclosure.get('length')
    +        episode_number, original_filename = self.url.split('/')[-2:]
    +        self.filename = f'{episode_number}-{original_filename}'
    +
    +    def download(self, download_path: Path):
    +        with requests.get(self.url, stream=True) as r:
    +            full_path = download_path / self.filename
    +            with full_path.open('wb') as fp:
    +                print(f'Writing file {self.filename}', flush=True)
    +                for chunk in r.iter_content():
    +                    fp.write(chunk)
    +
    +
    +def download_rss():
    +    print('Downloading RSS...', flush=True)
    +    res = requests.get(RSS_URL)
    +    return etree.XML(res.content)
    +
    +
    +def ensure_download_dir(download_dir: str):
    +    download_path = Path(download_dir)
    +    download_path.mkdir(parents=True, exist_ok=True)
    +    return download_path
    +
    +
    +def make_episodes(xml_root):
    +    enclosures = xml_root.xpath('//enclosure')
    +    return (Episode(enc) for enc in enclosures)
    +
    +
    +def find_missing(download_path: Path, episodes):
    +    for episode in episodes:
    +        episode_path = download_path / episode.filename
    +        try:
    +            existing_file_size = episode_path.stat().st_size
    +        except FileNotFoundError:
    +            yield episode
    +        # it might be partially downloaded, re-encoded or
    +        # anything wrong with the already downloaded episode
    +        if existing_file_size != episode.length:
    +            yield episode
    +
    +
    +def download_episodes(download_path: Path, episodes):
    +    for ep in episodes:
    +        print(f'Getting episode {ep.url}', flush=True)
    +        ep.download(download_path)
    +
    +
    +def parse_args():
    +    parser = argparse.ArgumentParser(description='Download Talk Python To Me podcast episodes to the given dir')
    +    parser.add_argument('--download-dir', default=os.environ.get('DOWNLOAD_DIR', 'episodes'))
    +    return parser.parse_args()
    +
    +
    +def main():
    +    args = parse_args()
    +    download_path = ensure_download_dir(args.download_dir)
    +    xml_root = download_rss()
    +    episodes = make_episodes(xml_root)
    +    missing_episodes = find_missing(download_path, episodes)
    +    download_episodes(download_path, missing_episodes)
    +    return 0
    +
    +
    +if __name__ == '__main__':
    +    sys.exit(main())
    
    Signed-off-by: Kiss György <kissgyorgy@me.com>
    
    Signed-off-by: Kiss György <kissgyorgy@me.com>
    
    Signed-off-by: Kiss György <kissgyorgy@me.com>
    
    Signed-off-by: Kiss György <kissgyorgy@me.com>

---
+++ talkpython.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3.6
+import os
+import sys
+import argparse
+from pathlib import Path
+import requests
+from lxml import etree
+
+
+RSS_URL = 'https://talkpython.fm/episodes/rss'
+
+
+class Episode:
+    def __init__(self, enclosure):
+        self.url = enclosure.get('url')
+        self.length = enclosure.get('length')
+        episode_number, original_filename = self.url.split('/')[-2:]
+        self.filename = f'{episode_number}-{original_filename}'
+
+    def download(self, download_path: Path):
+        with requests.get(self.url, stream=True) as r:
+            full_path = download_path / self.filename
+            with full_path.open('wb') as fp:
+                print(f'Writing file {self.filename}', flush=True)
+                for chunk in r.iter_content():
+                    fp.write(chunk)
+
+
+def download_rss():
+    print('Downloading RSS...', flush=True)
+    res = requests.get(RSS_URL)
+    return etree.XML(res.content)
+
+
+def ensure_download_dir(download_dir: str):
+    download_path = Path(download_dir)
+    download_path.mkdir(parents=True, exist_ok=True)
+    return download_path
+
+
+def make_episodes(xml_root):
+    enclosures = xml_root.xpath('//enclosure')
+    return (Episode(enc) for enc in enclosures)
+
+
+def find_missing(download_path: Path, episodes):
+    for episode in episodes:
+        episode_path = download_path / episode.filename
+        try:
+            existing_file_size = episode_path.stat().st_size
+        except FileNotFoundError:
+            yield episode
+        # it might be partially downloaded, re-encoded or
+        # anything wrong with the already downloaded episode
+        if existing_file_size != episode.length:
+            yield episode
+
+
+def download_episodes(download_path: Path, episodes):
+    for ep in episodes:
+        print(f'Getting episode {ep.url}', flush=True)
+        ep.download(download_path)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Download Talk Python To Me podcast episodes to the given dir')
+    parser.add_argument('--download-dir', default=os.environ.get('DOWNLOAD_DIR', 'episodes'))
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    download_path = ensure_download_dir(args.download_dir)
+    xml_root = download_rss()
+    episodes = make_episodes(xml_root)
+    missing_episodes = find_missing(download_path, episodes)
+    download_episodes(download_path, missing_episodes)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
