d7c26398ae310e237aed80fc3a6655613f53d0e5
György Kiss <gyorgy@duck.com>
2025-03-09 16:28:26 +0100

    devenv setup

--- devenv.nix
+++ devenv.nix
@@ -1,45 +1,43 @@
-{ pkgs, lib, config, inputs, ... }:
-
+{ ... }:
 {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  env = {
+    UV_PYTHON_DOWNLOADS = "never";
+  };
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [ ];
 
   # https://devenv.sh/languages/
-  # languages.rust.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+    uv.enable = true;
+    uv.sync.enable = true;
+  };
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  enterShell = ''
-    hello
-    git --version
-  '';
 
   # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
 
   # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
+  pre-commit.default_stages = [
+    "pre-push"
+    "manual"
+  ];
+  pre-commit.hooks = {
+    ruff.enable = true;
+    ruff-format.enable = true;
+    check-added-large-files.enable = true;
+    check-json.enable = true;
+    check-toml.enable = true;
+    check-yaml.enable = true;
+    trim-trailing-whitespace = {
+      enable = true;
+      excludes = [ ".*.md$" "xml/" ];
+    };
+    end-of-file-fixer.enable = true;
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
